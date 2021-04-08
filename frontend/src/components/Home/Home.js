0d47b232ee5a3c1f62f3a241084816b5e8eaa794
TianGoCode <tuan.vuhoang.bk@gmail.com>
2021-04-08 20:21:32 +0700

    last one

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -2,8 +2,9 @@ import React,{useContext} from "react";
 import NewDishes from './NewDishes'
 import { Carousel, Container } from 'react-bootstrap'
 
-const src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_178ab0c5717%20text%20%7B%20fill%3A%23ffffff%3Bfont-weight%3Anormal%3Bfont-family%3A-apple-system%2CBlinkMacSystemFont%2C%26quot%3BSegoe%20UI%26quot%3B%2CRoboto%2C%26quot%3BHelvetica%20Neue%26quot%3B%2CArial%2C%26quot%3BNoto%20Sans%26quot%3B%2C%26quot%3BLiberation%20Sans%26quot%3B%2Csans-serif%2C%26quot%3BApple%20Color%20Emoji%26quot%3B%2C%26quot%3BSegoe%20UI%20Emoji%26quot%3B%2C%26quot%3BSegoe%20UI%20Symbol%26quot%3B%2C%26quot%3BNoto%20Color%20Emoji%26quot%3B%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_178ab0c5717%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%2320232a%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22278.3203125%22%20y%3D%22221.3%22%3EThird%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E"
-
+const src1="https://www.lux-review.com/wp-content/uploads/2019/09/7-quirky-catering-ideas-for-corporate-events-1024x576.jpg"
+const src2="https://getcooking.ca/wp-content/uploads/2019/03/Team-Building-Events.jpg"
+const src3="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYZ5bWDTzSq_pINBC0KdYcW_VNC_RXLt7X-g&usqp=CAU"
 function Home() {
     return (
         <>
@@ -12,35 +13,35 @@ function Home() {
                     <Carousel.Item>
                         <img
                             className="d-block w-100"
-                            src={src}
+                            src={src1}
                             alt="First slide"
                         />
                         <Carousel.Caption>
-                            <h3>First slide label</h3>
-                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                            <h3>Tạo menu riêng của bạn</h3>
+                            <p>Đăng ký thành viên và tạo ngay menu của bạn!</p>
                         </Carousel.Caption>
                     </Carousel.Item>
                     <Carousel.Item>
                         <img
                             className="d-block w-100"
-                            src={src}
+                            src={src2}
                             alt="Second slide"
                         />
 
                         <Carousel.Caption>
-                            <h3>Second slide label</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            <h3>Tham gia cac hoạt động cộng đồng</h3>
+                            <p>Tìm kiếm bạn bè và tham dự ngay</p>
                         </Carousel.Caption>
                     </Carousel.Item>
                     <Carousel.Item>
                         <img
                             className="d-block w-100"
-                            src={src}
+                            src={src3}
                             alt="Third slide"
                         />
                         <Carousel.Caption>
-                            <h3>Third slide label</h3>
-                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                            <h3>Tìm kiếm các món ăn</h3>
+                            <p>Sự lựa chọn đầy đủ cho mỗi ngày</p>
                         </Carousel.Caption>
                     </Carousel.Item>
                 </Carousel>
--- frontend/src/components/Home/NavMenu.js
+++ frontend/src/components/Home/NavMenu.js
@@ -22,7 +22,7 @@ function NavMenu(props) {
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto">
                         <Nav.Link as={Link} to="/auth">Đăng nhập/Đăng ký</Nav.Link>
-                        <Nav.Link as={Link} to="/test">Phòng thí nghiệm</Nav.Link>
+
                     </Nav>
                 </Navbar.Collapse>
             </>)
@@ -39,7 +39,7 @@ function NavMenu(props) {
     return (
         <Navbar bg="light" expand="lg">
             <Container>
-                <Navbar.Brand as={Link} to="/">Cookpad</Navbar.Brand>
+                <Navbar.Brand as={Link} to="/">MyKitchen</Navbar.Brand>
                 <Form inline>
                     <FormControl type="text" placeholder="Tìm kiếm..." className="mr-sm-2" />
                     <Button variant="outline-warning"><span>&#128269;</span></Button>
